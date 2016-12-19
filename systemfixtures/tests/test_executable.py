964369c6e006f41c072be272a090d64cddd4c948
Free Ekanayaka <free.ekanayaka@gmail.com>
2016-12-19 15:57:32 +0100

    Fix flakiness in FakeExecutable tests

--- systemfixtures/tests/test_executable.py
+++ systemfixtures/tests/test_executable.py
@@ -11,10 +11,10 @@ from testtools.matchers import (
 from ..executable import FakeExecutable
 
 
-class FakeGroupsTest(TestCase):
+class FakeExecutableTest(TestCase):
 
     def setUp(self):
-        super(FakeGroupsTest, self).setUp()
+        super(FakeExecutableTest, self).setUp()
         self.executable = self.useFixture(FakeExecutable())
 
     def test_out(self):
@@ -28,23 +28,30 @@ class FakeGroupsTest(TestCase):
             self.executable.path,
             FileContains(matcher=Contains("time.sleep(1)")))
 
-    def test_listen(self):
+    def test_listen_random(self):
         self.executable.listen()
+        self.assertIsNotNone(self.executable.port)
+
+    def test_listen(self):
+        self.executable.listen(6666)
         self.executable.out("hello")
         process = subprocess.Popen(
-            [self.executable.path], stdout=subprocess.PIPE)
+            [self.executable.path], stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
         self.addCleanup(process.wait)
         self.addCleanup(process.kill)
-        # This ensure that the port will be open
+        # This ensures that the port will be open
         self.assertEqual(b"hello\n", process.stdout.read(6))
         sock = socket.socket()
         sock.connect(("127.0.0.1", self.executable.port))
         self.assertEqual("127.0.0.1", sock.getsockname()[0])
 
     def test_hang(self):
+        self.executable.out("hello")  # Used to ensure the process is running
         self.executable.hang()
         process = subprocess.Popen(
             [self.executable.path], stdout=subprocess.PIPE)
+        self.assertEqual(b"hello\n", process.stdout.read(6))
         process.terminate()
         self.addCleanup(process.wait)
         self.addCleanup(process.kill)
