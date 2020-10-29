9cd70001bcc49056bd0a667b784fb3645ae492d5
Natalia <nataliabidart@gmail.com>
2020-10-29 09:34:15 -0300

    Support DirEntry paths when checking is_fake_path
    When using shutil.copytree from a non-overlayed dir to an overlayed dir,
    copytree's implementation uses os.scandir that "returns an iterator of
    os.DirEntry objects corresponding to the entries in the directory given
    by path".
    
    The os.DirEntry does not support calling startswith on it directly, so
    this work handles that case by using it's name attribute instead.

--- systemfixtures/filesystem.py
+++ systemfixtures/filesystem.py
@@ -139,6 +139,8 @@ class FakeFilesystem(Fixture):
         def _is_fake_path_or_fd(self, path, *args, **kwargs):
             if isinstance(path, int):
                 path = self._path_from_fd(path)
+            elif isinstance(path, os.DirEntry):
+                path = path.name
             return self._is_fake_path(path)
 
     def _is_fake_symlink(self, src, dst, *args, **kwargs):
